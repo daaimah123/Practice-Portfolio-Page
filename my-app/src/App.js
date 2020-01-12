2b9ef7436c8e08fd47030d5e071bc676284e565e
daaimah123 <41805952+daaimah123@users.noreply.github.com>
2020-01-12 04:37:12 -0800

    clean up after some time

---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+
+      <header className="App">
+      {/* <header> TODO: would like to make a where's LOL doll react app, squashing for now to have basic understanding, then come back */} 
+        <img src="https://png.pngtree.com/thumb_back/fw800/back_pic/04/63/02/075870db16965c6.jpg" alt="cartoon park scene" width="150%" height="150%"/>
+      {/* </header> */}
+      </header>
+
+    </div>
+    )
+  }
+}
+
+export default App;
